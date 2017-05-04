6e188140e2fdf3b9efd8880c156221a8d2e57663
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2017-05-04 18:02:03 +0200

    Lesson 10 - From toy DOM Driver to real DOM Driver - Part 2

--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
 import xs from 'xstream';
 import fromEvent from 'xstream/extra/fromEvent';
 import { run } from '@cycle/run';
-
-function h(tagName, children) {
-  return {
-    tagName: tagName,
-    children: children,
-  };
-}
-
-function h1(children) {
-  return {
-    tagName: 'H1',
-    children: children,
-  };
-}
-
-function span(children) {
-  return {
-    tagName: 'SPAN',
-    children: children,
-  };
-}
+import { h, h1, span, makeDOMDriver } from '@cycle/dom';
 
 // Logic (functional)
 function main(sources) {
-  const mouseover$ = sources.DOM.selectEvents('span', 'mouseover');
+  const mouseover$ = sources.DOM.select('span').events('mouseover');
   const sinks = {
     DOM: mouseover$
       .startWith(null)
       .map(() =>
         xs.periodic(1000)
         .map(i =>
-          h1([
+          h1({ style: { background: 'red' } }, [
             span([
               `Seconds elapsed: ${i}`
             ])
@@ -49,38 +29,6 @@ function main(sources) {
 // sink: output (write) effects
 
 // Effects (imperative)
-function makeDOMDriver(mountSelector) {
-  return function DOMDriver(obj$) {
-    function createElement(obj) {
-      const element = document.createElement(obj.tagName);
-      obj.children
-        .filter(c => typeof c === 'object')
-        .map(createElement)
-        .forEach(c => element.appendChild(c));
-      obj.children
-        .filter(c => typeof c === 'string')
-        .forEach(c => element.innerHTML += c);
-      return element;
-    }
-
-    obj$.subscribe({
-      next: (obj) => {
-        const container = document.querySelector(mountSelector);
-        container.innerHTML = '';
-        const element = createElement(obj);
-        container.appendChild(element);
-      }
-    });
-    const DOMSource = {
-      selectEvents: function (tagName, eventType) {
-        return fromEvent(document, eventType)
-          .filter(e => e.target.tagName === tagName.toUpperCase());
-      }
-    };
-    return DOMSource;
-  };
-}
-
 function consoleLogDriver(msg$) {
   msg$.subscribe({
     next: msg => console.log(msg)
