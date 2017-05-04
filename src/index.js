cbe55d5d403a90935ca36f3b1039a0a2fce81d15
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2017-05-04 15:17:36 +0200

    Lesson 5 - Read effects from the DOM: click events

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import xs from 'xstream';
+import fromEvent from 'xstream/extra/fromEvent';
 
 // Logic (functional)
-function main() {
+function main(DOMSource) {
+  const click$ = DOMSource;
   return {
-    DOM: xs.periodic(1000)
-      .map(i => `Seconds elapsed ${i}`),
+    DOM: click$
+      .startWith(null)
+      .map(() =>
+        xs.periodic(1000)
+        .map(i => `Seconds elapsed ${i}`)
+      ).flatten(),
     Log: xs.periodic(2000)
       .map(i => 2 * i),
   };
 }
+
+// source: input (read) effects
+// sink: output (write) effects
 
 // Effects (imperative)
 function DOMDriver(text$) {
@@ -18,6 +27,8 @@ function DOMDriver(text$) {
       container.textContent = text;
     }
   });
+  const DOMSource = fromEvent(document, 'click');
+  return DOMSource;
 }
 
 function consoleLogDriver(msg$) {
@@ -26,11 +37,19 @@ function consoleLogDriver(msg$) {
   });
 }
 
+// bProxy = ...
+// a = f(bProxy)
+// b = g(a)
+// bProxy.imitate(b)
+
 function run(mainFn, drivers) {
-  const sinks = mainFn();
-  Object.keys(drivers).forEach(key => {
-    drivers[key](sinks[key]);
-  });
+  const proxyDOMSource = xs.create();
+  const sinks = mainFn(proxyDOMSource);
+  const DOMSource = drivers.DOM(sinks.DOM);
+  proxyDOMSource.imitate(DOMSource);
+  // Object.keys(drivers).forEach(key => {
+  //   drivers[key](sinks[key]);
+  // });
 }
 
 const drivers = {
