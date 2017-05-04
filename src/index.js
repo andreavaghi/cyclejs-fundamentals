59c035cd39e9ec57d45a84cd7134ef7250c0228d
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2017-05-04 18:16:42 +0200

    Lesson 11 - Hello World in Cycle.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,23 @@
-import xs from 'xstream';
-import fromEvent from 'xstream/extra/fromEvent';
 import { run } from '@cycle/run';
-import { h, h1, span, makeDOMDriver } from '@cycle/dom';
+import { label, input, h1, hr, div, makeDOMDriver } from '@cycle/dom';
 
-// Logic (functional)
 function main(sources) {
-  const mouseover$ = sources.DOM.select('span').events('mouseover');
-  const sinks = {
-    DOM: mouseover$
-      .startWith(null)
-      .map(() =>
-        xs.periodic(1000)
-        .map(i =>
-          h1({ style: { background: 'red' } }, [
-            span([
-              `Seconds elapsed: ${i}`
-            ])
-          ])
-        )
-      ).flatten(),
-    Log: xs.periodic(2000)
-      .map(i => 2 * i),
+  const inputEv$ = sources.DOM.select('.field').events('input');
+  const name$ = inputEv$.map(e => e.target.value).startWith('');
+  return {
+    DOM: name$.map(name =>
+      div([
+        label('Name: '),
+        input('.field', { type: 'text' }),
+        hr(),
+        h1(`Hello ${name}!`)
+      ])
+    )
   };
-  return sinks;
-}
-
-// source: input (read) effects
-// sink: output (write) effects
-
-// Effects (imperative)
-function consoleLogDriver(msg$) {
-  msg$.subscribe({
-    next: msg => console.log(msg)
-  });
 }
 
 const drivers = {
   DOM: makeDOMDriver('#app'),
-  Log: consoleLogDriver
 };
 
 run(main, drivers);
